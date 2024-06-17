b472f5d1efea2895dc72a8b2876f24fdfd893864
Twan <165776546+TKJdeB@users.noreply.github.com>
2024-06-17 19:27:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function verwacht(){
+        window.location.href = "/wacht.html";
+}
+function geleerd(){
+        window.location.href = "/geleerd.html";
+}
+function onthouden(){
+        window.location.href = "/onthouden.html";
+}
+function portret(){
+        window.location.href = "/portret.html";
+}
